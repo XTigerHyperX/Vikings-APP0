ff0a4dd2d48cd96e884f934f53a8fe409b2d8fc9
XTigerHyperX <xtigerhyperx@gmail.com>
2022-09-08 12:05:44 +0100

    Start screen

---
+++ Vikings/Forms/Start.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Vikings.Forms
+{
+    partial class Start
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Start));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Log_btn = new System.Windows.Forms.Button();
+            this.Reg_btn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 150);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(133, 86);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(82)))), ((int)(((byte)(191)))), ((int)(((byte)(124)))));
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(158, 375);
+            this.panel1.TabIndex = 1;
+            // 
+            // Log_btn
+            // 
+            this.Log_btn.Location = new System.Drawing.Point(396, 137);
+            this.Log_btn.Name = "Log_btn";
+            this.Log_btn.Size = new System.Drawing.Size(99, 38);
+            this.Log_btn.TabIndex = 2;
+            this.Log_btn.Text = "Login";
+            this.Log_btn.UseVisualStyleBackColor = true;
+            // 
+            // Reg_btn
+            // 
+            this.Reg_btn.Location = new System.Drawing.Point(396, 198);
+            this.Reg_btn.Name = "Reg_btn";
+            this.Reg_btn.Size = new System.Drawing.Size(99, 38);
+            this.Reg_btn.TabIndex = 3;
+            this.Reg_btn.Text = "Register";
+            this.Reg_btn.UseVisualStyleBackColor = true;
+            // 
+            // Start
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(769, 375);
+            this.Controls.Add(this.Reg_btn);
+            this.Controls.Add(this.Log_btn);
+            this.Controls.Add(this.panel1);
+            this.Name = "Start";
+            this.Text = "Start";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Panel panel1;
+        private Button Log_btn;
+        private Button Reg_btn;
+    }
+}
